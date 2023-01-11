6d6f72366be4510a3f3c11d2341a4628b8a05792
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-11 12:10:51 -0500

    commit tree 00f446888ebbf2b1a04576a06c42e9650e6a0c06

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,52 @@
-# your code goes here
+class Person
+
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+    def initialize (name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8 
+        @hygiene = 8
+    end
+    def happiness=(value)
+        if value >= 10
+            @happiness = 10
+        elsif value <= 0
+            @happiness = 0
+        else
+            @happiness = value
+        end
+    end
+    def hygiene=(amount)
+        if amount >= 10
+            @hygiene = 10
+        elsif amount <= 0
+            @hygiene = 0
+        else
+            @hygiene = amount
+        end
+    end
+    def happy?
+        self.happiness > 7
+    end
+    def clean?
+        self.hygiene > 7
+    end
+    def get_paid(amount)
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+    def take_bath(amount)
+        self.hygiene += amount
+        "a song"
+    end
+    def work_out
+
+    end
+    def call_friend
+
+    end
+    def start_conversation
+
+    end
+end
